bc5a9817eaef05463ddd0f1dd5004a5740b7ef21
mitch <daiutomitch@gmail.com>
2014-09-13 23:07:21 -0400

    StickyBot v2

---
+++ decends.py
@@ -0,0 +1,200 @@
+decends = [
+['Noah Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Wadatsumi Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Sandalphon Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Gaia Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Tengu!!!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Zeus Vulcan Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Hera-Sowilo Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Hera-Beorc Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Heracles Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Izanami Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Beelzebub Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Takeminakata Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Athena Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Hera-Ur Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Zeus-Dios Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Satan Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Hera-Is Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['The Thief Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['The Goddess Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Zeus Descended!',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Hero Descended',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+],
+['Hera Descended',
+"""
+Ayy it's noah
+
+noah
+
+noah
+"""
+]
+]
---
+++ sticky-bot.py
@@ -0,0 +1,83 @@
+import praw
+import requests
+import bs4
+from datetime import datetime, timedelta
+from decends import decends
+
+#Date suffix
+def ord(n):
+    return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
+
+#Updates the sidebar sticky every 3 days
+def updateSidebarSticky():
+    pad = r.get_subreddit("puzzleanddragons")
+    current_sidebar = pad.get_settings()['description']
+    idx1 = current_sidebar.find(START_TAG)
+    idx2 = current_sidebar.find(END_TAG)
+    if idx1 < 0 or idx2 < 0:
+        print "Not Found"
+
+    else:
+        start_idx = idx1 + len(START_TAG);
+        stickytext = current_sidebar[start_idx : idx2] 
+        idx3 = stickytext.find('/comments/')
+        if idx3 < 0:
+            print "Not Found"
+
+        else:
+            previous_submission_id = stickytext[idx3+len('/comments/'):].split('/')[0]     
+            submission = r.get_submission(submission_id = previous_submission_id)            
+            previous_time = datetime.fromtimestamp(submission.created_utc)
+            now_time = datetime.now()
+            hours_ago = (now_time - previous_time).days * 24 + (now_time - previous_time).seconds/3600
+            if (hours_ago >= 25):
+                future_date = now_time + timedelta(days=3)
+                title = 'General Help & Discussion Thread.  '+now_time.strftime('%b')+" "+ord(now_time.day)+" - "+future_date.strftime('%b')+" "+ord(future_date.day)
+                submit_result = r.submit(pad, title, text="###Post your questions or box help requests in this thread\n\n* Please use PadHerder to show your monster box.\n* Be specific with your questions so people have a better understanding of your needs.\n* Show some love for the people who took time to respond to your question; upvote any responses that were helpful.\n\nPrevious thread [here]("+submission.permalink+")")
+                new_sidebar = current_sidebar[0:start_idx] + "\n> ## ["+title+"]("+submit_result.permalink+"?sort=new#icon-exclamation-red)  **Individual posts will be removed. Please use this thread instead.**\n" + current_sidebar[idx2:]         
+                pad.update_settings(description=new_sidebar)
+                print "New post created: "+submit_result.permalink
+
+            else:
+                print "Skipping. Less than 3 days"
+
+#Updates the Decends Sticky
+def updateDecendsSticky():
+    pad = r.get_subreddit("puzzleanddragons")
+    now = datetime.now()
+    #Grab current PADwiki schedule
+    soup = bs4.BeautifulSoup(requests.get("http://pad.wikia.com/wiki/Event_Schedule").content)
+    #Get proper elements
+    rows = soup.find("table", {"id": "currentEvents"}).find_all("tr")
+    for row in rows:
+        cells = row.find_all("td")
+        try:
+            #Check Date
+            if str(cells[0].text) == now.strftime('%m/%d 00:00\n').lstrip("0"):
+                for decend in decends:
+                    #Check Decends
+                    if cells[2].find('a', title=decend[0]) != None:
+                        title = str(now.strftime('%m/%d').lstrip("0")) + " - " + decend[0] + " Discussion Thread"
+                        #Post new sticky
+                        post = r.submit(pad, title, text=decend[1])
+                        post.sticky()
+        except:
+            #exception required because of the table header
+            pass
+
+#Kick off main function
+def main():
+    global r
+    global START_TAG
+    global END_TAG
+    START_TAG = '> [](#s1)';
+    END_TAG = '> [](#s1_)';
+    r = praw.Reddit(user_agent='/r/PuzzleAndDragons Sidebar Updater')
+    r.config.decode_html_entities = True
+    r.login('username', 'password')
+    updateSidebarSticky()
+    updateDecendsSticky()
+
+
+if __name__ == '__main__':
+    main()
